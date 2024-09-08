f0c3dcb94938b2d588206f9de9f2db3c257d704f
Mayank <vijaymaynk2003@gmail.com>
2024-09-08 22:44:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-icon").addEventListener("click", function () {
+    document.querySelector(".sidebar").style.display = "flex";
+  });
+  
+  document.querySelector(".close-icon").addEventListener("click", function () {
+    document.querySelector(".sidebar").style.display = "none";
+  });
+  
